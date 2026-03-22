5014bcf39cdeba8165af3a0aeec06e8a2957f303
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-03-22 16:21:43 +0700

    Add scene name collection

---
+++ Assets/Scripts/ProjectScope/SceneName.cs
@@ -0,0 +1,10 @@
+namespace ProjectScope
+{
+    public struct SceneName
+    {
+        public const string Boot = "Boot";
+        public const string Loading = "Loading";
+        public const string Home = "Home";
+        public const string Gameplay = "Gameplay";
+    }
+}
