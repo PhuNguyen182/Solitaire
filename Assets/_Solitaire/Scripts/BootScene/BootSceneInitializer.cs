796267a0db18bbc5bba6a77f0f0da55a04f83aec
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-04-25 17:03:42 +0700

    Create addressable group

--- Assets/_Solitaire/Scripts/BootScene/BootSceneInitializer.cs
+++ Assets/_Solitaire/Scripts/BootScene/BootSceneInitializer.cs
@@ -1,9 +1,27 @@
+using ProjectScope;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 namespace _Solitaire.Scripts.BootScene
 {
     public class BootSceneInitializer : MonoBehaviour
     {
+        private ProjectInitializer _projectInitializer;
         
+        private void Awake()
+        {
+            this._projectInitializer = FindFirstObjectByType<ProjectInitializer>();
+            this.WaitAndLoadNextScene().Forget();
+        }
+
+        private async UniTask WaitAndLoadNextScene()
+        {
+            await UniTask.WaitUntil(AllServiceRegistered);
+            await SceneManager.LoadSceneAsync(SceneName.Loading);
+            return;
+
+            bool AllServiceRegistered() => this._projectInitializer.AllServiceRegistered;
+        }
     }
 }
